90bdcc643bce6d35059f346c67e3c1b784c5369c
rybai <rybai86@gmail.com>
2019-11-13 15:12:14 -0500

    up though apollo stuff

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,16 +1,25 @@
-export default (req, res) => {
-  res.status(200).json({
-    test: 'Hello Level Up',
-  });
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Hello Level Up!';
+    },
+  },
 };
 
-//Long Way
-// export default (req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.statusCode = 200;
-//   res.end(
-//     JSON.stringify({
-//       test: 'Hello Level Up',
-//     })
-//   );
-// };
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
